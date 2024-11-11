b1ea0b911743d9bbc3b9121549c04ee3f965f8f4
Guilherme Reis <guilhermereis2009@hotmail.com>
2024-11-11 04:47:10 -0300

    :card_file_box: raw: adiciona e altera os arquivos modelos User e ResultState.

--- app/src/main/java/com/picpay/desafio/android/data/local/User.kt
+++ app/src/main/java/com/picpay/desafio/android/data/local/User.kt
@@ -1,13 +1,16 @@
-package com.picpay.desafio.android
+package com.picpay.desafio.android.data.local
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
+@Entity(tableName = "user_table")
 @Parcelize
 data class User(
+    @PrimaryKey @SerializedName("id") val id: Int,
     @SerializedName("img") val img: String,
     @SerializedName("name") val name: String,
-    @SerializedName("id") val id: Int,
     @SerializedName("username") val username: String
 ) : Parcelable
